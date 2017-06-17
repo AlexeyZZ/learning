ba127eb5cbc673a95519578b078766e41a2c62f9
Alex <testerbox037@gmail.com>
2017-06-17 15:48:42 +0300

    new project with static

---
+++ Tests/SimpleClassExample/Motorcycle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Motorcycle
+    {
+        public int driverIntensity;
+        static string driverName;
+        public int Age;
+        string s;
+
+        // main ctor
+        public Motorcycle(int dIntencity,int age,string dname= "")
+        {
+            driverIntensity = dIntencity;
+            driverName = dname;
+            Age = age;
+        }
+        // chained ctor
+        public Motorcycle (int idencitty) : this(idencitty,11)
+        {
+            //some logic for age
+        }
+
+        public void SetDriverName(string name)
+        {
+            //both operators are equvalent
+            // this.driverName= name;
+            driverName = name;
+            
+            
+        }
+    }
+}
